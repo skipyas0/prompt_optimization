3788fe5078d1f02de36241fd0180d8c664dd8aa0
Vojtech Klouda <kloudvoj@fel.cvut.cz>
2024-09-10 12:11:50 +0200

    run scripts, response start in prompts, ready for hot tests

--- run.py
+++ run.py
@@ -5,33 +5,41 @@ from evoprompt import Prompt
 from vllm_api import OpenAIPredictor
 from datetime import datetime
 import random 
+from os import getenv
 
 if __name__ == "__main__":
-    rel_path = "tasks/entity_extraction/"
+    task_category = "entity_extraction"
+    rel_path = f"tasks/{task_category}/"
     task_name = "german-intelligence-says-russian-gru-group-behind-nato-eu-cyberattacks-2024-09-09"
 
     with open(f"{rel_path}/generation.txt") as f:
-        gen_instructions = "Hello world! This is a testy test."#f.read()
+        gen_instructions = f.read()
     with open(f"{rel_path}/samples/x/{task_name}.txt") as f:
-        task = "Generic task :O. What could it be?"#f.read()
+        task = f.read()
     with open(f"{rel_path}/samples/y/{task_name}.txt") as f:
         ground = f.read()
 
     model = "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4"
     # "mistralai/Mistral-7B-v0.3"
-    #api = OpenAIPredictor(model)
-    #prelude = [{"role": "system", "message": "You are a helpful assistant. You follow instructions and answer concisely."}]
-    #gen_handle = lambda x: api.predict(prelude, x)
+    api = OpenAIPredictor(model)
+    prelude = [{"role": "system", "content": "You are a helpful assistant. You follow instructions and answer concisely."}]
+    gen_handle = lambda x: api.predict(prelude, x)
 
-    #score = lambda x: simple_list_intersection(ground.split(','), x)
+    score = lambda x: simple_list_intersection(ground.split(','), x.split(','))
 
+    """ fast debug to replace LLM calls
+    gen_instructions = "Hello world! This is a testy test."
+    task = "Generic task :O. What could it be?"
     def scramble(input: str) -> str:
         char_list = list(input)
         random.shuffle(char_list)
         return ''.join(char_list)
     gen_handle = scramble
+    score = lambda _: random.random()
+    """
 
-    log_file = f"logs/{datetime.now().strftime('%H-%M-%S_%d-%m-%Y')}.txt"
+    ident = getenv("SLURM_JOB_ID") or datetime.now().strftime('%H-%M-%S_%d-%m-%Y')
+    log_file = f"logs/results/{task_category}_{ident}.txt"
 
     class MyPrompt(Prompt):
         def __init__(self, prompt: str) -> None:
@@ -44,7 +52,7 @@ if __name__ == "__main__":
 
         def _fitness(self) -> float:
             self.result = self.generate(self.traits[0] + task)
-            res = random.random()#score(self.result.split(','))
+            res = score(self.result)
             self.fitness = res
             self.log()
             return res
